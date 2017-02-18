1830b22fc61b6697e1752a073c83c99a29b46f98
beeduck <b33duck@gmail.com>
2017-02-18 11:11:35 -0800

    Insertion sort runner and formatted output.

--- Project_1/src/com/comp496/Main.java
+++ Project_1/src/com/comp496/Main.java
@@ -1,14 +1,11 @@
 package com.comp496;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class Main {
 
     private static final int randomSize = 1000000;
-    private static final int runCount = 5;
-    private static final int[] comps = {
+    private static final int[] mergeComps = {
             10,
             100,
             1000,
@@ -17,71 +14,81 @@ public class Main {
             1000000,
             5000000
     };
-
-
+    private static final int[] insertionComps = {
+            10,
+            100,
+            1000,
+            10000,
+            100000,
+            200000,
+            300000
+    };
 
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
-        List<ResultsOutput> resultsOutputList = new ArrayList<>();
-        ResultsOutput resultsOutput;
-        for(int i = 0; i < comps.length; i++) {
-            resultsOutput = new ResultsOutput(comps[i]);
-            resultsOutput.runSort();
-            resultsOutputList.add(resultsOutput);
+        int[] a;
+        long startTime;
+
+
+        // Run merge sort timer
+        long mergeTotalComp, mergeTotalTime, mergeAvgComp, mergeAvgTime;
+        mergeTotalComp = mergeTotalTime = mergeAvgComp = mergeAvgTime = 0;
+        for(int i = 0; i < mergeComps.length; i++) {
+            a = createList(mergeComps[i], randomSize);
+
+            startTime = System.nanoTime();
+            mergeTotalComp += Sorts.mergesort(a);
+            mergeTotalTime += System.nanoTime() - startTime;
+
+            mergeAvgComp = mergeTotalComp / mergeComps.length;
+            mergeAvgTime = mergeTotalTime / mergeComps.length;
+
+            double logOfN = Math.log10(mergeComps[i]) / Math.log10(2);
+            double comparisonComputed = mergeAvgComp / (mergeComps[i] * logOfN);
+            double timeComputed = mergeAvgTime / (mergeComps[i] * logOfN);
+
+            String format = "%-10d%d%f%d%f%n";
+            System.out.printf(format, mergeComps[i], mergeAvgComp, comparisonComputed, mergeAvgTime, timeComputed);
+
+//            System.out.println(Integer.toString(mergeComps[i]) + "\t" + mergeAvgComp +
+//                                "\t" + comparisonComputed + "\t" + mergeAvgTime + "\t" + timeComputed);
         }
 
-        resultsOutputList.forEach(s -> System.out.println(s));
+
+        // Run insertion sort timer
+        long insertionTotalComp, insertionTotalTime, insertionAvgComp, insertionAvgTime;
+        insertionTotalComp = insertionTotalTime = insertionAvgComp = insertionAvgTime = 0;
+        for(int i = 0; i < insertionComps.length; i++) {
+            a = createList(insertionComps[i], randomSize);
+
+            startTime = System.nanoTime();
+            insertionTotalComp += Sorts.insertionsort(a);
+            insertionTotalTime += System.nanoTime() - startTime;
+
+            insertionAvgComp = insertionTotalComp / insertionComps.length;
+            insertionAvgTime = insertionTotalTime / insertionComps.length;
+
+            double comparisonComputed = insertionAvgComp / (Math.pow(insertionComps[i], 2));
+            double timeComputed = insertionAvgTime / (Math.pow(insertionComps[i], 2));
+
+            System.out.println(Integer.toString(insertionComps[i]) + "\t" + insertionAvgComp +
+                    "\t" + comparisonComputed + "\t" + insertionAvgTime + "\t" + timeComputed);
+        }
 
         return;
     }
 
+    private static int[] createList(int arraySize, int randomSize) {
+        int[] list = new int[arraySize];
+        Random random = new Random();
 
-
-
-
-    private static class ResultsOutput {
-        public int n;
-        public long averageComp;
-        public long averageTime;
-        private static long totalTime = 0;
-        private static long totalComp = 0;
-
-        public ResultsOutput(int n) {
-            this.n = n;
+        for (int i = 0; i < arraySize; i++) {
+            list[i] = random.nextInt(randomSize);
         }
 
-        public void runSort() {
-            totalTime = totalComp = 0;
-            for (int i = 0; i < runCount; i++) {
-                int[] a = createList(n, randomSize);
-                runSortTimer(a);
-            }
-            averageComp = totalComp / runCount;
-            averageTime = totalTime / runCount;
-        }
-
-        private static void runSortTimer(int[] a) {
-            long startTime = System.nanoTime();
-            totalComp += Sorts.mergesort(a);
-            totalTime += System.nanoTime() - startTime;
-        }
-
-        private int[] createList(int arraySize, int randomSize) {
-            int[] list = new int[arraySize];
-            Random random = new Random();
-
-            for (int i = 0; i < arraySize; i++) {
-                list[i] = random.nextInt(randomSize);
-            }
-
-            return list;
-        }
-
-        @Override
-        public String toString() {
-            return Integer.toString(n) + "\t\t\t" + averageComp + "\t\t\t" + "todo" + "\t\t\t" + averageTime + "\t\t\t" + "todo";
-        }
+        return list;
     }
+
 }
